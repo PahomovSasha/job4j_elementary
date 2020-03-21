2cf684f434989b725a46ae6474531825052c6dcd
PahomovSasha <147852369caxap13>
2020-03-21 19:11:53 +0500

    4.1.2. Глупый бот. [#258824]

---
+++ src/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+
+    public static String answer(String question) {
+        String rsl;
+        if (question.equals("Привет, Бот.")) {
+            rsl = "Привет, умник.";
+        } else if (question.equals("Пока.")) {
+            rsl = "До скорой встречи.";
+        } else {
+            rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        }
+        return rsl;
+    }
+}
