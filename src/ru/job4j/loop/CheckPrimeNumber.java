5692e6cd8ed84d58cc546424e77d4d391cf0fb47
PahomovSasha <147852369caxap13>
2020-03-24 20:44:50 +0500

    5.5. Простое число. [#258908]

---
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+
+    public static boolean check(int number) {
+        boolean prime = false;
+        for (int i = 2; i < number - 1; i++) {
+            prime = number % i != 0;
+        }
+        return prime;
+    }
+}
