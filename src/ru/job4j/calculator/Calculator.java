31bc3b64a0556ef743a50a0a4e2f7cffba4670eb
PahomovSasha <147852369caxap13>
2020-03-24 22:46:20 +0500

    5.8. JavaDoc. Code convention. [#258915]

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -1,6 +1,16 @@
 package ru.job4j.calculator;
 
+/**
+ * Class Класс для вычисления арифмитических операций + / - *
+ * @author PahomovAV
+ * @since 20/03/2020
+ * @version 1
+ */
 public class Calculator {
+    /**
+     * Main.
+     * @param args - args
+     */
     public static void main(String[] args) {
         int one = 1;
         int two = 2;
