e09111e7edcd8d74e086a5ff642124317c3d123e
PahomovSasha <147852369caxap13>
2020-03-25 19:45:27 +0500

    4.1.0. Операторы сравнения в String. [#258830] Результат сравнения теперь сразу записывается в переменную access.

--- src/ru/job4j/condition/StringEq.java
+++ src/ru/job4j/condition/StringEq.java
@@ -3,12 +3,7 @@ package ru.job4j.condition;
 public class StringEq {
     public static boolean check(String login) {
         String root = "root";
-        boolean access;
-        if (root.equals(login)) {
-            access = true;
-        } else {
-            access = false;
-        }
+        boolean access = root.equals(login);
         return access;
     }
 
