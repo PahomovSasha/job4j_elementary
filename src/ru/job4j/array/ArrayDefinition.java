6e595ee4c743ecfcd7ac7572d2f7fc7fe713b8dd
PahomovSasha <147852369caxap13>
2020-03-25 21:40:50 +0500

    6.0.2. Заполнение массива. [#258852]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -6,5 +6,13 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Вася";
+        names[1] = "Николай";
+        names[2] = "Петр";
+        names[3] = "Саша";
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
