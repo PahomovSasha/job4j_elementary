b643000b171768090d38d1831e8849920e40e848
PahomovSasha <147852369caxap13>
2020-03-31 21:49:59 +0500

    2.2. Рефакторинг - Площадь треугольника. [#258991]

--- src/ru/job4j/condition/TriangleTest.java
+++ src/ru/job4j/condition/TriangleTest.java
@@ -6,14 +6,22 @@ import static org.junit.Assert.assertThat;
 
 public class TriangleTest {
     @Test
-    public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
-        assertThat(result, is(true));
+    public void whenTriangleExist() {
+        Point a = new Point(1, 1);
+        Point b = new Point(5, 9);
+        Point c = new Point(9, 1);
+        Triangle triangle = new Triangle(a, b, c);
+        double resultArea = triangle.area();
+        assertThat(resultArea, is(32.0));
     }
 
     @Test
-    public void exist() {
-        boolean result = Triangle.exist(1.0, 1.0, 2.0);
-        assertThat(result, is(false));
+    public void triangleNotExist() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        Point c = new Point(0, 3);
+        Triangle triangle = new Triangle(a, b, c);
+        double resultArea = triangle.area();
+        assertThat(resultArea, is(-1.0));
     }
 }
