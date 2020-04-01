d0d01ea425af13df4f2fd77286b22d40503c5a94
PahomovSasha <147852369caxap13>
2020-04-01 12:55:18 +0500

    3. Реализация профессий в коде. [#259010]

---
+++ src/ru/job4j/oop/Doctor.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Doctor extends Profession {
+    private String specialty;
+
+    public Diagnose heal(Pacient pacient) {
+        return null;
+    }
+}
---
+++ src/ru/job4j/oop/Surgeon.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Surgeon extends Doctor {
+    private int countOperation;
+
+    public int numberOperationsPerformed(Surgeon surgeon) {
+        return 0;
+    }
+}
