3f790e34dfa32b45da706ba888ad2c62d724cd89
PahomovSasha <caxap13@mail.ru>
2020-04-08 21:18:39 +0500

    0.3. Тестирование исключений с junit. [#258980]

---
+++ src/ru/job4j/ex/FactTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class FactTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenMinus5(){
+        int rsl = Fact.calc(-5);
+        assertThat(rsl, is("n must be > 0"));
+    }
+}
