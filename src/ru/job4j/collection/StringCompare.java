616d9a6592be4c46dce800b48c1349ebe7809162
PahomovSasha <caxap13@mail.ru>
2020-04-22 22:00:30 +0500

    3. Компаратор для строк [#258933]

--- src/ru/job4j/collection/StringCompare.java
+++ src/ru/job4j/collection/StringCompare.java
@@ -7,19 +7,18 @@ public class StringCompare implements Comparator<String> {
     public int compare(String left, String right) {
         char[] leftChar = left.toCharArray();
         char[] rightChar = right.toCharArray();
-        int size = Math.max(leftChar.length, rightChar.length);
-        try {
-            for (int i = 0; i < size; i++) {
-                int compareChar = Character.compare(leftChar[i], rightChar[i]);
-                if (compareChar > 0) {
-                    return 1;
-                } else if (compareChar < 0) {
-                    return -1;
-                }
+        int size = Math.min(leftChar.length, rightChar.length);
+        for (int i = 0; i < size; i++) {
+            int compareChar = Character.compare(leftChar[i], rightChar[i]);
+            if (compareChar > 0) {
+                return 1;
+            } else if (compareChar < 0) {
+                return -1;
             }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            return leftChar.length < rightChar.length ? -1 : 1;
         }
-        return 0;
+        if (leftChar.length == rightChar.length) {
+            return 0;
+        }
+        return leftChar.length > rightChar.length? 1 : -1;
     }
 }
