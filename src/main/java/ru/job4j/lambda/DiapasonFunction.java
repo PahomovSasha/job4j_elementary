881ebbf73d8c77a891a495f6b4961087364c48f3
PahomovSasha <caxap13@mail.ru>
2020-06-22 23:21:36 +0500

    Перемещение файлов

--- src/main/java/ru/job4j/lambda/DiapasonFunction.java
+++ src/main/java/ru/job4j/lambda/DiapasonFunction.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.function.Function;
 
 public class DiapasonFunction {
-    static List<Double> diapason(int start, int end, Function<Double, Double> func) {
+    public static List<Double> diapason(int start, int end, Function<Double, Double> func) {
         List<Double> result = new ArrayList<>();
         for (double i = start; i < end; i++) {
             result.add(func.apply(i));
--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -5,7 +5,7 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class School {
-    static List<Student> collect(List<Student> students, Predicate<Student> predict) {
+    public static List<Student> collect(List<Student> students, Predicate<Student> predict) {
     return students.stream().filter(predict).collect(Collectors.toList());
     }
 }
